6c45fa44bf5c4f6ddca20b491bf2e82fb579a567
SheilaAbby <sheilaabigael2@gmail.com>
2022-11-22 19:59:55 +0300

    Added Patch and Delete Endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 //Post Method
 router.post('/post',  async (req, res) => {
     if (!req.body.name && !req.body.age) {
-        return res.status(400).json({message: "Request Body Can Not be Empty" })
+        return res.status(400).json({message: "Request Body Can Not be Empty." })
     }
     const data = new Model({
         name: req.body.name,
@@ -35,13 +35,30 @@ router.get('/getOne/:id', async (req, res) => {
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const dataToUpdate = req.body
+        const options = {new: true}
+
+        const result = await Model.findByIdAndUpdate(id, dataToUpdate, options)
+        res.send(result)
+        }
+    catch {
+        res.status(400).json({message: "Error occured - NOT Updated"})
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.id} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 module.exports = router;
